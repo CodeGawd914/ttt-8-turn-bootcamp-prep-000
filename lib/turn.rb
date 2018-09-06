64074a7e7b8b7bdd6315021c56ec06d8a1edf48e
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-09-06 15:14:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,8 @@ def turn(board)
   input = gets.strip
     input_to_index(input)
     if valid_move?(board,input)
-      move(board,input)
+      move(board, input)
+      display_board
   else
     turn(board)
   end  
