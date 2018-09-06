7e83fcd98a698113ac9f0f6d6b63076dff183bbe
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-09-06 14:35:57 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+def display_board(board)
+  
+end
+
+def valid_move?(board, index)
+  if (index.between?(0,8) && !position_taken?(board,index))
+    true
+  else
+    false
+  end
+end
+
+def position_taken? (board,index)
+  taken = nil
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+end
